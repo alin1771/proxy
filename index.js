e9a3a11ecd22c9580e588687c0cdda7e598ec0ba
alin1771 <alin1771@yahoo.com>
2022-01-24 23:16:22 +0200

    lite code

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-const http = require('http');
 const https = require('https');
-
 
 const PROXY_HOST = 'graph.facebook.com';
 
-const server = http.createServer((req, res) => {
-    req.headers.host = PROXY_HOST;
-
-    if (req.url.length < 5) {
-        res.write("merge");
-        res.end();
-        return;
-    }
-
-    var proxy_req = https.request({
+require('http').createServer((req, res) => {
+    delete req.headers.host;
+    req.pipe(https.request({
         method: req.method,
         host: PROXY_HOST, //req.headers.proxy_host,
         path: req.url,
         port: 443,
         headers: req.headers
-    }, function (proxy_res) {
+    }, proxy_res => {
         res.writeHead(proxy_res.statusCode, proxy_res.headers);
         proxy_res.pipe(res);
-    })
-
-    req.pipe(proxy_req);
-});
-
-server.listen(process.env.PORT || 5540);
+    }));
+}).listen(process.env.PORT || 5540);
